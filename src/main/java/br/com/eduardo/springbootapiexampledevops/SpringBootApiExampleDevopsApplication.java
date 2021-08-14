a11e63ef8f837712146c159e569dc2e38b2997ca
Eduardo Rodrigues da Silva <silva.rodriguesedu@icloud.com>
2021-08-14 13:43:33 -0300

    Project Spring Boot Api example devops created

---
+++ src/main/java/br/com/eduardo/springbootapiexampledevops/SpringBootApiExampleDevopsApplication.java
@@ -0,0 +1,13 @@
+package br.com.eduardo.springbootapiexampledevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApiExampleDevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApiExampleDevopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/eduardo/springbootapiexampledevops/SpringBootApiExampleDevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.eduardo.springbootapiexampledevops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApiExampleDevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
