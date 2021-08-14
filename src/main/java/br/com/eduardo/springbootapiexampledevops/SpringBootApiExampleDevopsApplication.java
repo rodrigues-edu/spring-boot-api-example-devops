cdb12ffb1ef7d322f25f34eb544cdd19fb6cf65a
Eduardo Rodrigues da Silva <silva.rodriguesedu@icloud.com>
2021-08-14 20:33:23 -0300

    sampleData method created in man application

--- src/main/java/br/com/eduardo/springbootapiexampledevops/SpringBootApiExampleDevopsApplication.java
+++ src/main/java/br/com/eduardo/springbootapiexampledevops/SpringBootApiExampleDevopsApplication.java
@@ -1,7 +1,11 @@
 package br.com.eduardo.springbootapiexampledevops;
 
+import br.com.eduardo.springbootapiexampledevops.entity.ThemeParkRide;
+import br.com.eduardo.springbootapiexampledevops.repository.ThemeParkRideRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootApiExampleDevopsApplication {
@@ -10,4 +14,12 @@ public class SpringBootApiExampleDevopsApplication {
 		SpringApplication.run(SpringBootApiExampleDevopsApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner sampleData(ThemeParkRideRepository repository) {
+		return (args) -> {
+			repository.save(new ThemeParkRide("Rollercoaster", "Train ride that speeds you along.", 5, 3));
+			repository.save(new ThemeParkRide("Log flume", "Boat ride with plenty of splashes.", 3, 2));
+			repository.save(new ThemeParkRide("Teacups", "Spinning ride in a giant tea-cup.", 2, 4));
+		};
+	}
 }
