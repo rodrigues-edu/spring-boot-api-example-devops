2081a136acc448c7983a0f18f1253c17c666fae3
Eduardo Rodrigues da Silva <silva.rodriguesedu@icloud.com>
2021-08-14 19:31:22 -0300

    ThemeParkRideRepository class created

---
+++ src/main/java/br/com/eduardo/springbootapiexampledevops/repository/ThemeParkRideRepository.java
@@ -0,0 +1,12 @@
+package br.com.eduardo.springbootapiexampledevops.repository;
+
+import br.com.eduardo.springbootapiexampledevops.entity.ThemeParkRide;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ThemeParkRideRepository extends CrudRepository<ThemeParkRide, Long> {
+    List<ThemeParkRide> findByName(String name);
+}
